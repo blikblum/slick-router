cfeb5392912efeed23783e12c459bde023f1a295
Luiz Américo <luizamericop@gmail.com>
2017-12-14 18:18:49 -0300

    Change build script to use webpack directly and make cherrytree.js the main entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const path = require('path')
+
 module.exports = {
   context: __dirname,
+  entry: './lib/router.js',
   output: {
     library: 'cherrytree',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
+    filename: path.join('build', 'cherrytree.js')
   },
   resolve: {
     alias: {
