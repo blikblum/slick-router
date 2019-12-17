fab6566ec2cf3d606e416fbcbd6d517e89af652a
Luiz Américo <luizamericop@gmail.com>
2019-12-17 09:20:28 -0300

    Remove tests index file

--- tests/index.js
+++
@@ -1,9 +0,0 @@
-import './unit/dashTest'
-import './unit/linksTest'
-import './unit/pathTest'
-import './unit/routerTest'
-import './unit/functionDslTest'
-import './unit/arrayDslTest'
-
-import './functional/pushStateTest'
-import './functional/routerTest'
